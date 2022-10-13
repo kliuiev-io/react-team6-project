44db41fc02ee31ba2fc1d11281b219bea1547d6c
Illia Kliuiev <i.kliuiev@easygenerator.com>
2022-10-13 07:09:18 +0000

    Card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./styles.css";
+import { GetCard } from "./components/Card";
 
 export default function App() {
+  const card = GetCard({
+    image: "https://content1.rozetka.com.ua/goods/images/preview/242978958.jpg",
+    title: "test",
+    price: 100,
+  });
   return (
     <div className="App">
       <h1>Hello Branch 1</h1>
+      {card}
       <h2>Start editing to see some magic happen!</h2>
       <h2>Blah-blah-blah</h2>
     </div>
