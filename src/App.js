b7f7b5de0fe6daa65fac9f32edbfcbce21de79b6
Svistelnyk Oleksandr <113510457+SvistelnykOleksandr@users.noreply.github.com>
2022-10-13 10:47:32 +0300

    Get sample data (#3)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./styles.css";
 import { GetCard } from "./components/Card";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
   const card = GetCard({
     image: "https://content1.rozetka.com.ua/goods/images/preview/242978958.jpg",
     title: "test",
