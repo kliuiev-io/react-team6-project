3e29a0483803bacc4d1985d9d866d355a174216c
Illia Kliuiev <i.kliuiev@easygenerator.com>
2022-10-13 07:02:12 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello Branch 1</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h2>Blah-blah-blah</h2>
     </div>
   );
 }
